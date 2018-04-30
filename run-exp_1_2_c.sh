0c7055c90c40eceaaea88943b16e669c2b0943c4
mvishwali28 <mvishwali28@gmail.com>
2018-04-29 23:29:10 -0400

    Update run-exp_1_2_c.sh

--- run-exp_1_2_c.sh
+++ run-exp_1_2_c.sh
@@ -8,7 +8,7 @@
 #SBATCH --job-name=conservativity
 #SBATCH --mail-type=END
 #SBATCH --mail-user=netID@nyu.edu
-#SBATCH --output=exp_1_c.out
+#SBATCH --output=exp_1_2_c.out
 #SBATCH --gres=gpu:1
 
 # Activate the conda environment
@@ -19,4 +19,4 @@ PYTHONPATH=$PYTHONPATH:. python quant_verify_exp2.py --exp one_c --out_path data
 
 # The output will be dumped into a file called terminal_results2.run_context
 touch terminal_results_2.txt
-cat exp_1_c.out > terminal_results_2.txt
+cat exp_1_2_c.out > terminal_results_2.txt
