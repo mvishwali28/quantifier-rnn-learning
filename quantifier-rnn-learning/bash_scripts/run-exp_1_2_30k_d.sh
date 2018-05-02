f123b2f5cb4064345ece2c1dbfb84fc2a107ea3c
Vishwali <mvishwali28@gmail.com>
2018-05-01 22:20:29 -0400

    Bash scripts

---
+++ quantifier-rnn-learning/bash_scripts/run-exp_1_2_30k_d.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=144:00:00
+#SBATCH --mem=40GB
+#SBATCH --job-name=conservativity
+#SBATCH --mail-type=END
+#SBATCH --mail-user=netID@nyu.edu
+#SBATCH --output=exp_1_2_30k_d.out.txt
+#SBATCH --gres=gpu:1
+
+# Activate the conda environment
+source activate nlu
+
+# Run the training script
+PYTHONPATH=$PYTHONPATH:. python quant_verify_exp_1_2_30k.py --exp one_d --out_path data/30k/exp-1-d
---
+++ quantifier-rnn-learning/bash_scripts/run-exp_1_2_5k_a.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=144:00:00
+#SBATCH --mem=40GB
+#SBATCH --job-name=conservativity
+#SBATCH --mail-type=END
+#SBATCH --mail-user=netID@nyu.edu
+#SBATCH --output=exp_1_2_10k_a.out.txt
+#SBATCH --gres=gpu:1
+
+# Activate the conda environment
+source activate nlu
+
+# Run the training script
+PYTHONPATH=$PYTHONPATH:. python quant_verify_exp_1_2_5k.py --exp one_a --out_path data/5/exp-1-a
